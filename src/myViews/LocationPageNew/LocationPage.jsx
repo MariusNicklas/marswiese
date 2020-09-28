7478d6dd29938527da300827bef496e375c2e450
Marius Nicklas <m.nicklas@marswiese.at>
2020-09-28 10:47:13 +0200

    map in LoactionPage now working

--- src/myViews/LocationPageNew/LocationPage.jsx
+++ src/myViews/LocationPageNew/LocationPage.jsx
@@ -1,31 +1,102 @@
 import React from 'react';
+// nodejs library that concatenates classes
+import classNames from 'classnames';
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles';
+// @material-ui/icons
+import PinDrop from '@material-ui/icons/PinDrop';
+import Phone from '@material-ui/icons/Phone';
+// core components
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
+import InfoArea from 'components/InfoArea/InfoArea.js';
+import Footer from 'components/Footer/Footer.js';
 
 import OlMap from 'ol/Map';
 import OlView from 'ol/View';
 import OlLayerTile from 'ol/layer/Tile';
 import OlSourceOsm from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
 
 import { MapComponent } from '@terrestris/react-geo';
 import './LocationPageStyles.scss';
+
+import contactUsStyle from 'assets/jss/material-kit-pro-react/views/contactUsStyle.js';
+
+const marswieseLatLon = [16.2767598, 48.2394908];
+const marswieseWebMercator = fromLonLat(marswieseLatLon);
 
 const layer = new OlLayerTile({
   source: new OlSourceOsm()
 });
 
-const center = [48.2394908, 16.2767598];
-
 const map = new OlMap({
   view: new OlView({
-    center: center,
-    zoom: 16
+    center: marswieseWebMercator,
+    zoom: 18
   }),
   layers: [layer]
 });
 
+const useStyles = makeStyles(contactUsStyle);
+
 const LocationPage = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <MapComponent map={map} />
+    <div>
+      <div className={classes.bigMap}>
+        <MapComponent map={map} />
+      </div>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.contactContent}>
+          <div className={classes.container}>
+            <h4 className={classes.title}>
+              Herzlich Willkommen im Sportzentrum Marswiese
+            </h4>
+            <GridContainer>
+              <GridItem md={6} sm={6}>
+                <p>
+                  Das Sportzentrum Marswiese liegt am Rande des Wienerwaldes im
+                  17. Wiener Gemeindebezirk. Eingebettet in einer idyllischen
+                  Hügellandschaft mitten im Grünen, bietet die Anlage jede Menge
+                  Möglichkeiten Sportarten aller Art auszuüben.
+                  <br />
+                  <br />
+                </p>
+              </GridItem>
+              <GridItem md={4} sm={4} className={classes.mlAuto}>
+                <InfoArea
+                  className={classes.info}
+                  title="Finde uns am Mars"
+                  description={
+                    <p>
+                      Neuwaldegger Str. 57a, <br /> 1700 Wien
+                    </p>
+                  }
+                  icon={PinDrop}
+                  iconColor="primary"
+                />
+                <InfoArea
+                  className={classes.info}
+                  title="Ruf uns an"
+                  description={<p>+43 1 489 7172</p>}
+                  icon={Phone}
+                  iconColor="primary"
+                />
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
+      </div>
+      <Footer
+        content={
+          <div>
+            <div className={classes.right}>
+              &copy; {1900 + new Date().getYear()} Marswiese
+            </div>
+          </div>
+        }
+      />
     </div>
   );
 };
